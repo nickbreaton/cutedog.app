08105bb34989d9a11986ac6011c1c80ea977d185
Nick Breaton <nick@nickbreaton.com>
2023-07-07 23:07:44 -0400

    Migrate coords store to own file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,7 @@ import { Content } from "~/lib/components/Content";
 import { getCityState } from "~/lib/maps";
 import { imageSize } from "image-size";
 import https from "node:https";
+import { createCoordsStore } from "~/lib/signals/coords";
 
 export function routeData() {
   return createServerData$(async (): Promise<InteractionResult[]> => {
@@ -60,31 +61,6 @@ export function routeData() {
 
     return Promise.all(interactionResultsPromises);
   });
-}
-
-function createCoordsStore() {
-  const [store, set] = createStore({ lat: 0, lon: 0 });
-
-  createComputed(() => {
-    if (isServer) {
-      return;
-    }
-
-    let timeout: NodeJS.Timeout;
-
-    function update() {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude: lat, longitude: lon } = position.coords;
-        set({ lat, lon });
-        timeout = setTimeout(update, 1000);
-      });
-    }
-
-    update();
-    onCleanup(() => clearTimeout(timeout));
-  });
-
-  return store;
 }
 
 function balaneQuoteText(text: string) {
