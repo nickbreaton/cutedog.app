97403d65a0a789e70790f8ad5c1272d09d2cb49a
Nick Breaton <nick@nickbreaton.com>
2023-07-12 21:05:51 -0400

    Fix uneven breakpoint

--- src/lib/components/Content.tsx
+++ src/lib/components/Content.tsx
@@ -1,5 +1,5 @@
 import { JSX } from "solid-js";
 
 export function Content(props: { children: JSX.Element }) {
-  return <div class="m-auto max-w-lg">{props.children}</div>;
+  return <div class="sm:m-auto sm:max-w-lg">{props.children}</div>;
 }
