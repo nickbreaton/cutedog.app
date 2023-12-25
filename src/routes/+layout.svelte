4b895e576437eb06dacf86dc07cf3f7bc50c6219
Nick Breaton <nick@nickbreaton.com>
2023-12-24 21:50:10 -0500

    add global layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<slot />
+
+<style>
+	:global(body) {
+		font-family:
+			-apple-system,
+			BlinkMacSystemFont,
+			Segoe UI,
+			Roboto,
+			Oxygen-Sans,
+			Ubuntu,
+			Cantarell,
+			sans-serif;
+	}
+</style>
