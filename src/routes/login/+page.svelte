46889a7368aebe32cf086c9f4ee8354da8bef52b
Nick Breaton <nick@nickbreaton.com>
2023-12-24 21:44:14 -0500

    fix typing

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 
-	let { form } = $props<{ form?: { error: string } }>();
+	let { form } = $props();
 </script>
 
 <form method="POST" action="?/login" use:enhance>
