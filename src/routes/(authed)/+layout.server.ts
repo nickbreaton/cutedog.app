83b42f4d666283430c3a07f4d31ea5843a11ac6c
Nick Breaton <nick@nickbreaton.com>
2023-12-26 21:13:59 -0500

    ignore redirect param when going to root

--- src/routes/(authed)/+layout.server.ts
+++ src/routes/(authed)/+layout.server.ts
@@ -5,7 +5,10 @@ import { withParams } from '$lib/params';
 
 export const load: LayoutServerLoad = (event) => {
 	if (!isAuthed(event.cookies)) {
-		const params = new URLSearchParams([['redirect', event.url.pathname]]);
+		const params =
+			event.url.pathname.length > 1
+				? new URLSearchParams([['redirect', event.url.pathname]])
+				: new URLSearchParams();
 		return redirect(307, withParams('/login', params));
 	}
 };
