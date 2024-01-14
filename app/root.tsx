538bcf987d99a30eadfc4105bc6c8216c9f712d2
Nick Breaton <nick@nickbreaton.com>
2024-01-13 22:11:13 -0500

    feat: dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,7 @@ export default function App() {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="color-scheme" content="light dark" />
 				<Meta />
 				<Links />
 			</head>
